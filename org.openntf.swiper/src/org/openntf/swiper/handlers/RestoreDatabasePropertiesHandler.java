e158c9801f2d41d6f2cfc39061df4f6195423807
Cameron Gregor <camerongregor@gmail.com>
2015-10-19 14:33:37 +1100

    Added a prompt to specify prefix. Saving iconnote and dbicon as well restoring as well

--- org.openntf.swiper/src/org/openntf/swiper/handlers/RestoreDatabasePropertiesHandler.java
+++ org.openntf.swiper/src/org/openntf/swiper/handlers/RestoreDatabasePropertiesHandler.java
@@ -1,25 +1,167 @@
 package org.openntf.swiper.handlers;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.window.Window;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+import com.ibm.commons.ResourceHandler;
+import com.ibm.commons.util.StringUtil;
+import com.ibm.commons.util.io.ByteStreamCache;
+import com.ibm.commons.util.io.StreamUtil;
+import com.ibm.designer.domino.ide.resources.DominoResourcesPlugin;
+import com.ibm.designer.domino.ide.resources.NsfException;
+import com.ibm.designer.domino.ide.resources.jni.NotesDesignElement;
+import com.ibm.designer.domino.ide.resources.util.NsfUtil;
 import com.ibm.designer.domino.team.action.AbstractTeamHandler;
+import com.ibm.designer.domino.team.util.SyncUtil;
 
 public class RestoreDatabasePropertiesHandler extends AbstractTeamHandler {
+
+	private final String NSF_FILE_DBPROPS = "AppProperties/database.properties";
+	private final String NSF_FILE_DBICON = "AppProperties/$DBIcon";
+	private final String NSF_FILE_ICONNOTE = "Resources/IconNote";
+
+	private final String SAVE_FILE_DBPROPS = "Swiper/%s.database.properties";
+	private final String SAVE_FILE_DBICON = "Swiper/%s.$DBIcon";
+	private final String SAVE_FILE_ICONNOTE = "Swiper/%s.IconNote";
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
 		processSelectedProject(HandlerUtil.getCurrentSelection(event));
 
-		if (this.desProject != null) {
-			System.out.println("Would Restore App Properties for" + this.desProject.getProject().getName());
+		IProgressMonitor monitor = new NullProgressMonitor();
 
+		try {
+			InputDialog dialog = new InputDialog(getShell(), "Codename", "Type the codename", "testing", null);
+
+			if (dialog.open() != Window.OK) {
+				return null;
+			}
+
+			String prefix = dialog.getValue();
+
+			if (StringUtil.isEmpty(prefix)) {
+				return null;
+			}
+			
+			replaceDatabaseProperties(prefix, monitor);
+
+			replaceIconNote(prefix, monitor);
+
+			replaceDBIcon(prefix, monitor);
+
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 		return null;
-		
+
+	}
+
+	private boolean replaceDatabaseProperties(String prefix, IProgressMonitor monitor) throws CoreException {
+
+		String sourceFileName = String.format(SAVE_FILE_DBPROPS, prefix);
+
+		IFile nsfFile = this.desProject.getProject().getFile(NSF_FILE_DBPROPS);
+		IFile diskFile = this.desProject.getProject().getFile(sourceFileName);
+
+		return replaceNsfContents(diskFile, nsfFile, monitor);
+
+	}
+
+	private boolean replaceIconNote(String prefix, IProgressMonitor monitor) throws CoreException {
+
+		String sourceFileName = String.format(SAVE_FILE_ICONNOTE, prefix);
+
+		IFile nsfFile = this.desProject.getProject().getFile(NSF_FILE_ICONNOTE);
+		IFile diskFile = this.desProject.getProject().getFile(sourceFileName);
+
+		return replaceNsfContents(diskFile, nsfFile, monitor);
+
+	}
+
+	private boolean replaceDBIcon(String prefix, IProgressMonitor monitor) throws CoreException {
+
+		String sourceFileName = String.format(SAVE_FILE_DBICON, prefix);
+
+		IFile nsfFile = this.desProject.getProject().getFile(NSF_FILE_DBICON);
+		IFile diskFile = this.desProject.getProject().getFile(sourceFileName);
+
+		return replaceNsfContents(diskFile, nsfFile, monitor);
+
+	}
+
+	private boolean replaceNsfContents(IFile diskFile, IFile nsfFile, IProgressMonitor monitor) throws CoreException {
+
+		if (!diskFile.exists() || !nsfFile.exists()) {
+			System.out.println("For this to work, both NSF and DISK File need to exist");
+			return false;
+		}
+
+		int saveOptions = 0;
+
+		IProgressMonitor paramIProgressMonitor = new NullProgressMonitor();
+
+		boolean needsLock = false;
+		boolean lockResult = false;
+
+		NotesDesignElement localNotesDesignElement = DominoResourcesPlugin.getNotesDesignElement(nsfFile);
+
+		ByteStreamCache localByteStreamCache = new ByteStreamCache();
+
+		try {
+
+			needsLock = NsfUtil.needsImplicitLock(localNotesDesignElement);
+
+			if (needsLock) {
+				lockResult = NsfUtil.performLockOperation(localNotesDesignElement);
+			}
+
+			if ((!needsLock) || (lockResult)) {
+
+				InputStream localInputStream = diskFile.getContents();
+				StreamUtil.copyStream(localInputStream, localByteStreamCache.getOutputStream());
+				localInputStream.close();
+
+				// this.saveOptions is something to do with postProcessing /
+				// maybe update timestamp?
+				localNotesDesignElement.saveSyncContent(saveOptions, localByteStreamCache.toByteArray(),
+						paramIProgressMonitor);
+
+				localNotesDesignElement.refreshMetadata();
+
+				SyncUtil.setModifiedBySync(nsfFile);
+				SyncUtil.setSyncTimestamp(diskFile);
+			}
+			SyncUtil.logToConsole("locked " + lockResult + ":" + diskFile);
+			if (lockResult) {
+				NsfUtil.performUnLockOperation(localNotesDesignElement);
+			}
+			return true;
+		} catch (IOException localIOException) {
+			SyncUtil.logErrorToConsole(
+					StringUtil.format(ResourceHandler.getString("ImportSyncOperation.Errorimporting0.4"),
+							new Object[] { diskFile }),
+					localIOException);
+		} catch (NsfException localNsfException) {
+			SyncUtil.logErrorToConsole(
+					StringUtil.format(ResourceHandler.getString("ImportSyncOperation.Errorimporting0.5"),
+							new Object[] { diskFile }),
+					localNsfException);
+		}
+		return false;
+
 	}
 
 }
--- org.openntf.swiper/src/org/openntf/swiper/handlers/SaveDatabasePropertiesHandler.java
+++ org.openntf.swiper/src/org/openntf/swiper/handlers/SaveDatabasePropertiesHandler.java
@@ -1,113 +1,133 @@
 package org.openntf.swiper.handlers;
 
-import java.io.File;
 import java.io.InputStream;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.window.Window;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.openntf.swiper.action.FilterMetadataAction;
 
+import com.ibm.commons.util.StringUtil;
 import com.ibm.designer.domino.ide.resources.DominoResourcesPlugin;
 import com.ibm.designer.domino.ide.resources.NsfException;
 import com.ibm.designer.domino.ide.resources.jni.NotesDesignElement;
-import com.ibm.designer.domino.ide.resources.metamodel.MetaModelCategory;
-import com.ibm.designer.domino.ide.resources.metamodel.MetaModelDescriptor;
-import com.ibm.designer.domino.ide.resources.metamodel.MetaModelRegistry;
-import com.ibm.designer.domino.ide.resources.project.DominoDesignerProject;
 import com.ibm.designer.domino.team.action.AbstractTeamHandler;
-import com.ibm.misc.IOUtils;
 
 public class SaveDatabasePropertiesHandler extends AbstractTeamHandler {
+
+	private final String NSF_FILE_DBPROPS = "AppProperties/database.properties";
+	private final String NSF_FILE_ICONNOTE = "Resources/IconNote";
+	private final String NSF_FILE_DBICON = "AppProperties/$DBIcon";
+
+	private final String SAVE_FILE_DBPROPS = "Swiper/%s.database.properties";
+	private final String SAVE_FILE_ICONNOTE = "Swiper/%s.IconNote";
+	private final String SAVE_FILE_DBICON = "Swiper/%s.$DBIcon";
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
 		processSelectedProject(HandlerUtil.getCurrentSelection(event));
 
-		IProgressMonitor monitor = new NullProgressMonitor();
-		
 		if (this.desProject != null) {
 
-			IFile localIfile = this.desProject.getProject().getFile("AppProperties/database.properties");
-			
-			IFile destfile = this.desProject.getProject().getFile("Swiper/test.properties");
-			
-			if (localIfile.exists()) {
+			InputDialog dialog = new InputDialog(getShell(), "Codename", "Type the codename", "testing", null);
 
+			if (dialog.open() != Window.OK) {
+				return null;
 			}
-						
-			NotesDesignElement element = DominoResourcesPlugin.getNotesDesignElement(localIfile);
-			
-			try {
-				InputStream is = element.fetchSyncContent(0, monitor);
 
-				if (destfile.exists()) {
-					destfile.setContents(is, 0, monitor);
-				} else {
-					
-					
-					if (!destfile.getParent().exists() && destfile.getParent() instanceof IFolder) {
-						
-						IFolder folder = (IFolder) destfile.getParent();						
-						folder.create(false, false, monitor);			
-						
-					}
-					
-					
-					destfile.create(is, 0, monitor);
-				}						
-				
-				FilterMetadataAction.filterDiskFile(destfile, monitor);
-								
-			} catch (NsfException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (CoreException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			String prefix = dialog.getValue();
+
+			if (StringUtil.isEmpty(prefix)) {
+				return null;
 			}
 			
+			IProgressMonitor monitor = new NullProgressMonitor();
+
+			saveDatabaseProperties(prefix, monitor);
+
+			saveIconNote(prefix, monitor);
+
+			saveDBIcon(prefix, monitor);
+
 		}
-		
-//		IProgressMonitor monitor = new NullProgressMonitor();
-//		NotesDesignElement dbprops = null;
-//		
-//		IFile fileSaveProps = new File("C:\\Users\\cgregor\\Desktop\\TestSwiper");
-//
-//		try {
-//			
-//			InputStream is = dbprops.fetchSyncContent(0, monitor);
-//
-//			if (fileSaveProps.exists()) { // Update
-//				try {
-//					fileSaveProps.setContents(is, 0, monitor);
-//				} catch (CoreException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-//			} else { // create
-//				try {
-//					fileSaveProps.create(is, 0, monitor);
-//				} catch (Exception e) {
-//					// Couldn't save it
-//				}
-//			}
-//
-//		} catch (NsfException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 
 		return null;
+
+	}
+
+	private boolean saveDatabaseProperties(String prefix, IProgressMonitor monitor) {
+
+		String destFileName = String.format(SAVE_FILE_DBPROPS, prefix);
+
+		IFile localIfile = this.desProject.getProject().getFile(NSF_FILE_DBPROPS);
+		IFile destfile = this.desProject.getProject().getFile(destFileName);
+
+		return saveElement(localIfile, destfile, monitor);
+
+	}
+
+	private boolean saveIconNote(String prefix, IProgressMonitor monitor) {
+
+		String destFileName = String.format(SAVE_FILE_ICONNOTE, prefix);
+
+		IFile localIfile = this.desProject.getProject().getFile(NSF_FILE_ICONNOTE);
+		IFile destfile = this.desProject.getProject().getFile(destFileName);
+
+		return saveElement(localIfile, destfile, monitor);
+
+	}
+
+	private boolean saveDBIcon(String prefix, IProgressMonitor monitor) {
+
+		String destFileName = String.format(SAVE_FILE_DBICON, prefix);
+
+		IFile localIfile = this.desProject.getProject().getFile(NSF_FILE_DBICON);
+		IFile destfile = this.desProject.getProject().getFile(destFileName);
+
+		return saveElement(localIfile, destfile, monitor);
+
+	}
+
+	private boolean saveElement(IFile localIfile, IFile destfile, IProgressMonitor monitor) {
+
+		NotesDesignElement element = DominoResourcesPlugin.getNotesDesignElement(localIfile);
+
+		try {
+			InputStream is = element.fetchSyncContent(0, monitor);
+
+			if (destfile.exists()) {
+				destfile.setContents(is, 0, monitor);
+			} else {
+
+				if (!destfile.getParent().exists() && destfile.getParent() instanceof IFolder) {
+
+					IFolder folder = (IFolder) destfile.getParent();
+					folder.create(false, false, monitor);
+
+				}
+
+				destfile.create(is, 0, monitor);
+			}
+
+			FilterMetadataAction.filterDiskFile(destfile, monitor);
+
+		} catch (NsfException e) {
+			e.printStackTrace();
+			return false;
+		} catch (CoreException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
 
 	}
 
